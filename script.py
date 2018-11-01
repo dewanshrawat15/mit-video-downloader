b398c67cef747d821843c9d4249b8615192f816b
Dewansh <dewanshrawat15@gmail.com>
2018-11-01 18:04:39 +0530

    Script Working Fine

--- script.py
+++ script.py
@@ -1,25 +1,32 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen, urljoin
+import urllib
 
-def download_page(url):
+def download_video(video_rel_url, name):
+	download = urllib.request.FancyURLopener()
+	download.retrieve(video_rel_url, name+".mp4")
+	print(""+name+" downloaded")
+
+def download_page(url, name):
 	page = urlopen(url)
 	parsed_page = BeautifulSoup(page, 'html.parser')
-	downloadlinks_list = parsed_page.findAll("a")
-	print(downloadlinks_list)
+	req_link = parsed_page.find("a", text='Internet Archive').get("href")
+	download_video(req_link, name)
 
-def scrape_links(link):
-	src = urlopen(link)
+def scrape_links(sc_link):
+	src = urlopen(sc_link)
 	codebase = BeautifulSoup(src, 'html.parser')
 	url_list = codebase.findAll("a", attrs={"class": "medialink"})
 	for i in url_list:
+		name = i.getText()
 		extracted_url = i.get("href")
-		exact_url = urljoin(link, extracted_url)
-		download_page(exact_url)
+		exact_url = urljoin(sc_link, extracted_url)
+		download_page(exact_url, name)
 
-def verify(link):
+def verify(pr_link):
 	base = "https://ocw.mit.edu"
-	if base in link:
-		scrape_links(link)
+	if base in pr_link:
+		scrape_links(pr_link)
 	else:
 		print("The following url is not a MIT-OCW link")
 
