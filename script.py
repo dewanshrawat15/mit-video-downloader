c5e8dd05e35e3e425e099840b0d583f082c98962
Dewansh <dewanshrawat15@gmail.com>
2018-11-01 15:37:28 +0530

    Basic URL opener

--- script.py
+++ script.py
@@ -1,1 +1,6 @@
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
+
+src = urlopen("https://dewanshrawat.me/lastofus/")
+codebase = BeautifulSoup(src, 'html.parser')
+print(codebase.title.string)
