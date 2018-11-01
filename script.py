fdf531d431cc33b00076cb66b2cb3649c8c1f92c
Dewansh <dewanshrawat15@gmail.com>
2018-11-01 16:17:27 +0530

    Integrating functions together

--- script.py
+++ script.py
@@ -1,6 +1,30 @@
 from bs4 import BeautifulSoup
-from urllib.request import urlopen
+from urllib.request import urlopen, urljoin
 
-src = urlopen("https://dewanshrawat.me/lastofus/")
-codebase = BeautifulSoup(src, 'html.parser')
-print(codebase.title.string)
+def download_page(url):
+	page = urlopen(url)
+	parsed_page = BeautifulSoup(page, 'html.parser')
+	downloadlinks_list = parsed_page.findAll("a")
+	print(downloadlinks_list)
+
+def scrape_links(link):
+	src = urlopen(link)
+	codebase = BeautifulSoup(src, 'html.parser')
+	url_list = codebase.findAll("a", attrs={"class": "medialink"})
+	for i in url_list:
+		extracted_url = i.get("href")
+		exact_url = urljoin(link, extracted_url)
+		download_page(exact_url)
+
+def verify(link):
+	base = "https://ocw.mit.edu"
+	if base in link:
+		scrape_links(link)
+	else:
+		print("The following url is not a MIT-OCW link")
+
+def main():
+	link = input("Enter Download URL: ")
+	verify(link)
+
+main()
